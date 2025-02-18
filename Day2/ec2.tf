613d54bde5ce4d1184b08fa002e78c583da7b26e
kaxxsh <kameshwaran2002@gmail.com>
2025-02-18 15:00:55 +0530

    Replace EC2 instance resource with S3 bucket resource in ec2.tf

--- Day2/ec2.tf
+++ Day2/ec2.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "this" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "this" {
-  ami           = data.aws_ami.this.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "kamesh-pipeline-instance"
-  }
+resource "aws_s3_bucket" "this" {
+  bucket = "my-no-cost-terraform-bucket"
 }
